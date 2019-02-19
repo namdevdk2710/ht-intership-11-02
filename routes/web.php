df9553f473e0350a7b79fee6c3f2e975850ad7e5
yentyty <nguyenthiyen.tk20@gmail.com>
2019-02-19 15:12:25 +0700

    BE Template

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ Route::namespace('V1\Web\frontend')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/accommodation', 'RoomController@index')->name('room');
 });
-//// backend routes
+// backend routes
+
 Route::get('admin/home',function(){
     return view('backend.home.index');
 });
