4c09ef65303c6c09490cb38184984c726b6082c0
Tan Manh <manhvuongtan.it@gmail.com>
2019-02-19 10:01:33 +0700

    Template FE

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-/**
- * Frontend routes
- */
+// Frontend routes
 Route::namespace('V1\Web\frontend')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/accommodation', 'RoomController@index')->name('room');
