1b7c8fc52f7c97c038c04405daf6c0da10770b69
Tan Manh <manhvuongtan.it@gmail.com>
2019-02-19 10:04:04 +0700

    Template FE

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,3 @@ Route::namespace('V1\Web\frontend')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/accommodation', 'RoomController@index')->name('room');
 });
-
-
