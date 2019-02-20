dd726c87e161effc036f9ed0cd0ca48875b052b3
yentyty <nguyenthiyen.tk20@gmail.com>
2019-02-20 14:41:16 +0700

    BE List Banners

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ Route::group(['namespace' => 'V1\Web\frontend'], function () {
     Route::get('/', 'HomeController@index')->name('home');
 
     Route::get('/accommodation', 'RoomController@index')->name('room');
- });
+});
 
 // backend routes
 Route::group(['prefix' => '/admin', 'namespace' => 'V1\Web\backend'], function () {
