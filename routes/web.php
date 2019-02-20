353b117f9ef53e8703d4c1229da8e27a9025397a
Tan Manh <manhvuongtan.it@gmail.com>
2019-02-20 14:54:54 +0700

    Delete banners

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 // Frontend routes
-Route::namespace('V1\Web\frontend')->group(function () {
+Route::group(['namespace' => 'V1\Web\frontend'], function () {
     Route::get('/', 'HomeController@index')->name('home');
+
     Route::get('/accommodation', 'RoomController@index')->name('room');
 });
+
+
 // backend routes
-Route::get('admin/home', function () {
-    return view('backend.home.index');
+Route::group(['prefix' => '/admin', 'namespace' => 'V1\Web\backend'], function () {
+   Route::get('/home', function () {
+       return view('backend.home.index');
+   });
+
+   Route::resource('/banner', 'BannerController');
 });
