7e8804bf02345e6eb5cb65a43ab6dd9695dbac20
yentyty <nguyenthiyen.tk20@gmail.com>
2019-02-25 15:54:35 +0700

    be edit banner

--- app/Repositories/V1/Banner/BannerRepository.php
+++ app/Repositories/V1/Banner/BannerRepository.php
@@ -41,7 +41,7 @@ class BannerRepository extends BaseRepository implements BannerRepositoryInterfa
         if (!empty($data['image'])) {
             $file = $data['image'];
             $nameImageOld = 'uploads/images/banners/' . $banner->image;
-            if(file_exists(public_path($nameImageOld))) {
+            if( file_exists(public_path($nameImageOld))) {
                 unlink(public_path($nameImageOld));
             }
             $forder = ('uploads/images/banners');
@@ -49,8 +49,7 @@ class BannerRepository extends BaseRepository implements BannerRepositoryInterfa
             $fileName = $data['slug'] . '-' . time() . '.' . $extensionFile;
             $file->move($forder, $fileName);
             $data['image'] = $fileName;
-        }
-        else {
+        }else {
             $data['image'] = $banner->image;
         }
 
