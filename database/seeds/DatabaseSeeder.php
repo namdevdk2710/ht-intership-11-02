a518193b3bcb185622fc715d51bebb5514a1b9dc
Tan Manh <manhvuongtan.it@gmail.com>
2019-02-28 18:09:42 +0700

    Create migration model seed of table introduces

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CuisinesTableSeeder::class);
         $this->call(CuisineDetailsTableSeeder::class);
         $this->call(AboutsTableSeeder::class);
+        $this->call(IntroducesTableSeeder::class);
     }
 }
