bd200a3d9cb16a20b0cf367a1364faa04c5501fd
yentyty <nguyenthiyen.tk20@gmail.com>
2019-03-04 14:48:10 +0700

    BE create user

--- app/Http/Requests/Users/CreateUserRequest.php
+++ app/Http/Requests/Users/CreateUserRequest.php
@@ -28,7 +28,7 @@ class CreateUserRequest extends FormRequest
             'password' => 'required|min:3|max:32',
             'phone' => 'required|max:10|min:5',
             'email' => 'required|email|unique:users,email',
-            'passwordAgain'=>'required|same:password',
+            'passwordAgain' => 'required|same:password',
             'avatar' => 'image|max:2048',
         ];
     }
@@ -43,11 +43,11 @@ class CreateUserRequest extends FormRequest
             'password.required' => 'Vui lòng nhập trường này',
             'password.min' => 'Mật khẩu phải có ít nhất 3 ký tự',
             'password.max' => 'Mật khẩu chỉ được tối đa 32 ký tự',
-            'passwordAgain.required'=>'Vui lòng nhập trường này',
-            'passwordAgain.same'=>'Mật khẩu nhập lại chưa khớp',
+            'passwordAgain.required' => 'Vui lòng nhập trường này',
+            'passwordAgain.same' => 'Mật khẩu nhập lại chưa khớp',
             'phone.required' => 'Vui lòng nhập trường này',
             'phone.max' => 'Số điện thoại chỉ được tối đa 10 số',
-             'phone.min' => 'Số điện thoại không được nhỏ hơn 5 số'
+            'phone.min' => 'Số điện thoại không được nhỏ hơn 5 số',
             'avatar.image' => 'Định dạng không đúng file ảnh',
             'avatar.max' => 'Vui lòng nhập đúng kích thước ảnh',
         ];
