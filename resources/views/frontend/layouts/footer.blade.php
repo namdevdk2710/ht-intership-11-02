ae02c9db5e337ac16271dc56753c759e3e79a15b
Tan Manh <manhvuongtan.it@gmail.com>
2019-03-05 15:48:10 +0700

    Index page gallery

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,17 +1,17 @@
-<div class="section_footer_as clearfix">
+<div class="section_footer_as clearfix scrolling-item">
     <div class="row copyright mg0">
         <div class="col-md-3 text-left">
             <div class="footer_logo">
-                <a href="vn.html"><img class="img-responsive" src="{{asset('frontend/images/UploadImages/logo_grey.png')}}" /></a>
+                <a href="http://amarinresort.com.vn/vn"><img class="img-responsive" src="http://amarinresort.com.vn/local/logo/logo_grey.png" /></a>
             </div>
         </div>
         <div class="col-md-3 text-left">
             <div class="footer_logo text_foter">
                 <span class="text_foter_header">AMARIN RESORT PHÚ QUỐC</span><br/>
                 <span><span style="font-size:16px;"><span style="font-family:roboto_condensedlight;">Đường Trần Hưng Đạo, Thị Trấn Dương Đông,<br />
-  Phú Quốc, Kiên Giang<br />
-  Điện thoại: (84 297) 6259 666<br />
-  Email: info@amarinresort.com.vn</span></span>
+Phú Quốc, Kiên Giang<br />
+Điện thoại: (84 297) 6259 666<br />
+Email: info@amarinresort.com.vn</span></span>
                 </span>
             </div>
         </div>
@@ -19,14 +19,14 @@
             <div class="footer_logo text_foter">
                 <span class="text_foter_header">VĂN PHÒNG ĐẠI DIỆN HCM</span><br/>
                 <span><span style="font-size:16px;"><span style="font-family:roboto_condensedlight;">Phòng 09, Tầng 14, Tòa nhà Vincom,<br />
-  72 Lê Thánh Tôn, Phường Bến Nghé, TP.HCM<br />
-  Điện thoại: (84 8) 6868 0245<br />
-  Email: sales@amarinresort.com.vn<br />
-  <br />
-  VĂN PHÒNG ĐẠI DIỆN HÀ NỘI</span></span><br />
+72 Lê Thánh Tôn, Phường Bến Nghé, TP.HCM<br />
+Điện thoại: (84 8) 6868 0245<br />
+Email: sales@amarinresort.com.vn<br />
+<br />
+VĂN PHÒNG ĐẠI DIỆN HÀ NỘI</span></span><br />
                 <span style="font-size:16px;"><span style="font-family:roboto_condensedlight;">10, Đặng Xuân Bảng, Phường Đại Kim,<br />
-  Quận Hoàng Mai, Hà Nội<br />
-  Điện thoại: (84 24) 3540 1988</span></span>
+Quận Hoàng Mai, Hà Nội<br />
+Điện thoại: (84 24) 3540 1988</span></span>
                 </span>
             </div>
         </div>
@@ -34,19 +34,18 @@
             <div class="footer_logo">
                 <div class="text-center clearfix">
                     <div class="social_text">KẾT NỐI VỚI&nbsp;
-                        <a href="https://www.facebook.com/AmarinresortPQ/" target="_blank"><img src="frontend/icon_social/facebook.png" alt="Facebook" title="Facebook" /></a>
-                        <a href="#"><img src="frontend/icon_social/google.png" alt="Google" title="Google" /></a>
-                        <a href="#"><img src="frontend/icon_social/instagram.png" alt="Instagram" title="Instagram" /></a>
+                        <a href="https://www.facebook.com/AmarinresortPQ/" target="_blank"><img src="{{asset('../frontend/icon_social/facebook.png" alt="Facebook')}}" title="Facebook" /></a>
+                        <a href="#"><img src="{{asset('../frontend/icon_social/google.png')}}" alt="Google" title="Google" /></a>
+                        <a href="#"><img src="{{asset('../frontend/icon_social/instagram.png')}}" alt="Instagram" title="Instagram" /></a>
                     </div>
                 </div>
                 <div class="text-center clearfix">
-                    <div class="icon_bottom_menu_ward"><img src="frontend/images/icon_menu.png" /></div>
+                    <div class="icon_bottom_menu_ward"><img src="{{asset('../frontend/images/icon_menu.png')}}" /></div>
                 </div>
             </div>
         </div>
         <div class="clearfix menu_footer text-center" style="padding-bottom:5px;width:100%;clear:both">
-            <a href="vn/welcome/about-us.html">VỀ AMARIN</a> <span>|</span> <a href="vn/accommodation.html">PHÒNG</a> <span>|</span> <a href="vn/gallery.html">THƯ VIỆN</a> <span>|</span> <a href="vn/welcome/sitemap.html">SITEMAP</a> <span>|</span>
-            <a href="vn/contact.html">LIÊN HỆ</a>
+            <a href="http://amarinresort.com.vn/vn/welcome/about-us">VỀ AMARIN</a> <span>|</span> <a href="http://amarinresort.com.vn/vn/accommodation">PHÒNG</a> <span>|</span> <a href="http://amarinresort.com.vn/vn/gallery">THƯ VIỆN</a> <span>|</span>            <a href="http://amarinresort.com.vn/vn/welcome/sitemap">SITEMAP</a> <span>|</span> <a href="http://amarinresort.com.vn/vn/contact">LIÊN HỆ</a>
         </div>
         <div class="clearfix pull-left" style="padding-top:5px;width:100%;text-align:center;background-color:#c1c6cf">
             <p class="text_foter">
@@ -56,7 +55,6 @@
     </div>
 </div>
 <!-- Javascript files-->
-
 <script src="{{asset('../frontend/js/jquery-1.11.0.min.js')}}"></script>
 <script src="{{asset('../frontend/js/jquery-ui.min.js')}}"></script>
 <script src="{{asset('../frontend/js/jquery.easing.min.js')}}"></script>
@@ -64,8 +62,72 @@
 <script src="{{asset('../frontend/js/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
 <script src="{{asset('../frontend/js/jquery.cookie.js')}}"></script>
 <script src="{{asset('../frontend/js/front_as.js')}}"></script>
-<script src="{{asset('../frontend/js/accom_animate.js')}}"></script>
-
+<script src="{{asset('../frontend/js/dinning_animate.js')}}"></script>
+<script src="{{asset('../frontend/js/gallery_animate.js')}}"></script>
+<script language="javascript ">
+    $(function() {
+        $(".dinning ").addClass("active ");
+    });
+</script>
+<script language="javascript ">
+    $(window).on('load resize ', function() {
+        if ($(window).width() > 768) {
+            //Option 1
+            var hopt1s = $(".option1 ").map(function() {
+                return $(this).height()
+            }).get();
+            var maxItemHeightOpt1 = Math.max.apply(null, hopt1s);
+            $(".option1 ").animate({
+                height: maxItemHeightOpt1 + "px "
+            }, 500, function() {});
+            /*
+            $('.option1 ').css({
+            	'height ': maxItemHeightOpt1
+            });
+            */
+            //Option 2
+            var hopt2s = $(".option2 ").map(function() {
+                return $(this).height()
+            }).get();
+            var maxItemHeightOpt2 = Math.max.apply(null, hopt2s);
+            $(".option2 ").animate({
+                height: maxItemHeightOpt2 + "px "
+            }, 500, function() {
+                var el = $(this).find('.content-full-height ');
+                var cbv = el.height();
+                if (cbv > 0 && cbv != null) {
+                    var abc = maxItemHeightOpt2 - cbv;
+                    el.css({
+                        'padding-top ': (abc / 3) + "px "
+                    });
+                }
+            });
+            /*
+            $('.option2 ').css({
+            		'height ': maxItemHeightOpt2
+            });
+            */
+            //Option 3
+            var hopt3s = $(".option3 ").map(function() {
+                return $(this).height()
+            }).get();
+            var maxItemHeightOpt3 = Math.max.apply(null, hopt3s);
+            $('.option3 ').css({
+                'height ': maxItemHeightOpt3
+            });
+        } else {
+            $('.option1 ').css({
+                'height ': 'auto '
+            });
+            $('.option2 ').css({
+                'height ': 'auto '
+            });
+            $('.option3 ').css({
+                'height ': 'auto '
+            });
+        }
+    });
+</script>
 </body>
 
 </html>
