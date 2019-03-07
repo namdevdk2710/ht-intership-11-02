50274b5f33b82eea4475f3f564f5e2fb7a877a60
Tan Manh <manhvuongtan.it@gmail.com>
2019-03-07 10:58:03 +0700

    Edit template backend

--- resources/views/backend/home/index.blade.php
+++ resources/views/backend/home/index.blade.php
@@ -1,86 +1,76 @@
 @extends('backend.layouts.master')
-@section('content')
-<main class="app-content">
-        <div class="app-title">
-            <div>
-                <h1>
-                    <i class="fa fa-dashboard"></i> Dashboard
-                </h1>
-                <p>A free and open source Bootstrap 4 admin template</p>
-            </div>
-            <ul class="app-breadcrumb breadcrumb">
-                <li class="breadcrumb-item">
-                    <i class="fa fa-home fa-lg"></i>
-                </li>
-                <li class="breadcrumb-item">
-                    <a href="#">Dashboard</a>
-                </li>
-            </ul>
-        </div>
-        <div class="row">
-            <div class="col-md-6 col-lg-3">
-                <div class="widget-small primary coloured-icon">
-                    <i class="icon fa fa-users fa-3x"></i>
-                    <div class="info">
-                        <h4>Users</h4>
-                        <p>
-                            <b>5</b>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3">
-                <div class="widget-small info coloured-icon">
-                    <i class="icon fa fa-thumbs-o-up fa-3x"></i>
-                    <div class="info">
-                        <h4>Likes</h4>
-                        <p>
-                            <b>25</b>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3">
-                <div class="widget-small warning coloured-icon">
-                    <i class="icon fa fa-files-o fa-3x"></i>
-                    <div class="info">
-                        <h4>Uploades</h4>
-                        <p>
-                            <b>10</b>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3">
-                <div class="widget-small danger coloured-icon">
-                    <i class="icon fa fa-star fa-3x"></i>
-                    <div class="info">
-                        <h4>Stars</h4>
-                        <p>
-                            <b>500</b>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="tile">
-                    <h3 class="tile-title">Monthly Sales</h3>
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <canvas class="embed-responsive-item" id="lineChartDemo"></canvas>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="tile">
-                    <h3 class="tile-title">Support Requests</h3>
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <canvas class="embed-responsive-item" id="pieChartDemo"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-    </main>
+@section('content')
+
+<main class="app-content">
+    <div class="app-title">
+        <div>
+            <h1>
+                <i class="fa fa-home"></i> Home
+            </h1>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item">
+                <i class="fa fa-home fa-lg"></i>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="{{route('home.index')}}">Home</a>
+            </li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="tile">
+                <h3 class="tile-title">
+                    <a href="{{route('contact.index')}}">Contact</a>
+                </h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Full Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($contacts as $key => $contact)
+                        <tr>
+                            <td>{{ $key + 1 }}</td>
+                            <td>{{ str_limit($contact["title"], 50) }}</td>
+                            <td>{{ $contact->fullname }}</td>
+                            <td>{{ $contact->email }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="tile">
+                <h3 class="tile-title">
+                    <a href="{{route('offer.index')}}">Offer</a>
+                </h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Content</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($offers as $key => $offer)
+                        <tr>
+                            <td>{{ $key + 1 }}</td>
+                            <td>{{ $offer->name }}</td>
+                            <td>{{ str_limit($offer["content"], 50) }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</main>
 @endsection
